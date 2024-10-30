d1ed25179fb6f51fbaf27d656267b58a0ec58083
Enrico Legumi <93256992+EnricoLGM@users.noreply.github.com>
2024-10-30 13:12:19 +0100

    ADD: File dal repository dell'esercizio

---
+++ README.md
@@ -0,0 +1,2 @@
+# Esercizio2TutoratoGizzi
+Carica la soluzione dell' esercizio2. Usa il tuo nome e cognome come Nome del file
---
+++ enricoLegumi.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool chk_rettangolo(int a, int b, int c);
+
+int main() {
+    int a, b, c;
+    printf("Inserisci il valore di a: ");
+    scanf("%d", &a);
+    printf("\n");
+    printf("Inserisci il valore di b: ");
+    scanf("%d", &b);
+    printf("\n");
+    printf("Inserisci il valore di c: ");
+    scanf("%d", &c);
+    printf("\n");
+    if((a>0) && (b>0) && (c>0)) {
+        if((a<(b+c)) && (b<(a+c)) && (c<(a+b))) {
+            if((a>((b>c)?b-c:c-b)) && (b>((a>c)?a-c:c-a)) && (c>((a>b)?a-b:b-a))) {
+                printf("Condizione verificate, controllo del tipo di triangolo\n");
+                if((a!=b) && (a!=c) && (b!=c)) {
+                    printf("Il triangolo è scaleno\n");
+                    return 0;
+                }
+                if((a==b) && (a==c)) {
+                    printf("Il triangolo è equilatero\n");
+                    return 0;
+                }
+                if((a==b) || (b==c) || (a==c)) {
+                    printf("Il triangolo è isoscele\n");
+                    return 0;
+                }
+                /*
+                if() {
+                    printf("Il triangolo è rettangolo\n");
+                    return 0;
+                }
+                */
+            }
+            else {
+                printf("Almeno un lato inserito è minore della differenza degli altri due, uscita dal programma\n");
+            }
+        }
+        else {
+            printf("Almeno un lato inserito è maggiore della somma degli altri due, uscita dal programma\n");    
+        }
+    }
+    else {
+        printf("Almeno un lato inserito è negativo, uscita dal programma\n");
+    }
+}
+
+bool chk_rettangolo(int a, int b, int c) {
+    return true;
+}
